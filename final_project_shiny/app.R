1ca51b4c785f7a920a16a5fe27547bcf86052734
cjp12 <cjp12@uw.edu>
2019-03-10 15:24:14 -0700

    final project, formatting applied

--- final_project_shiny/app.R
+++ final_project_shiny/app.R
@@ -1,48 +1,80 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
+my_ui <- fluidPage(
+  
+  titlePanel("The World's Emotions at a Glance"),
+  
+  
+  
+  
+  
+  sidebarLayout( #this pannel will be taking in all of the inputs
+    #---------------------------------------------------------------------------------------------------    
+    sidebarPanel(
+      #-------------------------------------------------------------
+      sliderInput(inputId = ),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+      
+      selectInput(inputId = )
+      
+      
+      
+      
+      
+      
+      
+      #------------------------------------------------------------- 
+    ),
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    mainPanel(  #this is the display pannel.  This is where all the renderings will be taking place
+      #-------------------------------------------------------------
+      
+      tabsetPanel(
+        #-------------------------------------------------------------
+        tabPanel("tab1"
+                 
+                 
+        ),
+        
+        tabPanel("tab2"
+                 
+        ),
+        
+        tabPanel("tab3"
+                 
+                 
+                 
+        )
+        
+        #-------------------------------------------------------------  
       )
-   )
+      
+      #-------------------------------------------------------------
+    )
+    
+    
+    
+    
+    
+    
+    #----------------------------------------------------------------------------------------------------   
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+   
 }
 
 # Run the application 
